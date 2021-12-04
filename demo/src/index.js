e2471ce0ef0951c6cc424e8ddce41547253b19e9
uriyang <girlstoryk@gmail.com>
2021-12-04 15:45:56 +0900

    [demo] abstract

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,20 @@
-const element = document.getElementById('app');
-element.innerHTML = `
-  <p>Hello, world!</p>
-`;
+function createElement(tagName, ...children) {
+  const element = document.createElement(tagName);
+  children.forEach((child) => {
+    element.appendChild(child);
+  });
+  return element;
+}
+
+document.getElementById('app').appendChild(createElement(
+  'div',
+  createElement(
+    'p',
+    document.createTextNode('Hello, world!!!'),
+    document.createTextNode('Hello, world!!!!'),
+  ),
+  createElement(
+    'p',
+    document.createTextNode('Hi'),
+  ),
+));
