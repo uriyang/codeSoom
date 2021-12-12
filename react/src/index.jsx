8b04174375cda6e50217969f916f984870f02e18
uriyang <girlstoryk@gmail.com>
2021-12-12 23:24:14 +0900

    [react] add Function & props

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = function ({ children }) {
+  return (
+    <button type="button">
+      {children}
+    </button>
+  );
+};
+
+const Buttons = function () {
+  return (
+    <p>
+      {[1, 2, 3].map((i) => (
+        <Button>{i}</Button>
+      ))}
+    </p>
+  );
+};
+
 const App = function () {
   return (
     <div>
       <p>Hello, world!</p>
+      <p>Hi!</p>
+      <Buttons />
     </div>
   );
 };
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
